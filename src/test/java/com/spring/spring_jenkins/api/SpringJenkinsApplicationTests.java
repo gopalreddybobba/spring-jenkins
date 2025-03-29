5f64058e576af9d1e7f38d1aa708ca45c7aaf4c0
gopalreddybobba <gopalreddy.bobba@gmail.com>
2025-03-29 13:17:15 +0530

    commit 6

--- src/test/java/com/spring/spring_jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/spring/spring_jenkins/api/SpringJenkinsApplicationTests.java
@@ -15,7 +15,7 @@ class SpringJenkinsApplicationTests {
 	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	public  contextLoads() {
+	void contextLoads() {
 		logger.info("Test case executing......");
 		logger.info("Test case executing....2222..");
 		assertEquals(true,true);

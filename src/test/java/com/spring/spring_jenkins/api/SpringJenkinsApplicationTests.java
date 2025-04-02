5de8b0975721994fa09e35b9c71869649c5678df
Gopal Reddy <109059101+gopalreddybobba@users.noreply.github.com>
2025-04-02 11:32:05 +0530

    Update SpringJenkinsApplicationTests.java

--- src/test/java/com/spring/spring_jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/spring/spring_jenkins/api/SpringJenkinsApplicationTests.java
@@ -17,7 +17,7 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing......");
-		logger.info("Test case executing....2222..");
+		logger.info("Test case executing....444444444444444..");
 		assertEquals(true,true);
 	}
 
